0940d9cb8c873e5935db559a8a2a141bfdbeaef3
penguin_lv <penguin_lv@qq.com>
2024-02-02 23:06:18 +0800

    '函数重载'

--- section1/48overloadingfun.cpp
+++ section1/48overloadingfun.cpp
@@ -2,12 +2,23 @@
 
 using namespace std;
 
-int main(){
+void greet(string firstname){
+    cout<<"Hello, " << firstname <<endl;
+}
 
+void greet(string firstname, string lastname){
+    cout<<"Hello, " << firstname <<" "<<lastname<<endl;
+}
+
+int main(){
     
-    
+    greet("Penguin");
+    greet("Penguin","Lv");
+
     getchar();
     getchar();
 
     return 0;   
 }
+
+//函数重载要遵守函数签名的规则 ：函数名+（参数数量和类型） 满足参数数量和类型不同即可实现函数重载。
